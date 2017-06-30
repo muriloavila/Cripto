b29c46fc4ee2cdfb262f21cb6a0d5415622c63f0
Murilo Nogueira de Avila <murilo.avila068@gmail.com>
2017-06-30 10:42:18 -0300

    teste

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 // var jshint = require('gulp-jshint');
 // var uglify = require('gulp-uglify');
-// var convertEncoding = require('gulp-convert-encoding');
+var convertEncoding = require('gulp-convert-encoding');
 
 
 
@@ -16,20 +16,16 @@ var gulp = require('gulp');
 
 
 //compressor
-// gulp.task('uglify', function() {
-//     gulp.src('*.js')
-//     .pipe(convertEncoding({from: 'ISO-8859-1', to: 'UTF-8'}))
-//     .pipe(uglify())
-//     .pipe(convertEncoding({from: 'UTF-8', to: 'ISO-8859-1'}))
-//     .pipe(gulp.dest('dist'));
-// });
+gulp.task('uglify', function() {
+    gulp.src('*.js')
+    .pipe(convertEncoding({from: 'ISO-8859-1', to: 'UTF-8'}))
+    .pipe(uglify())
+    .pipe(convertEncoding({from: 'UTF-8', to: 'ISO-8859-1'}))
+    .pipe(gulp.dest('dist'));
+});
 
 
-// gulp.task('default',['uglify'], function() {
-//     console.log('Deu Certo');
-// });
-
-
-gulp.task('default', function() {
+gulp.task('default',['uglify'], function() {
     console.log('Deu Certo');
 });
+
