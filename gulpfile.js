510ff1c78b8a84c55f2ba0248cfc328b4f7b23d8
Murilo Nogueira de Avila <murilo.avila068@gmail.com>
2017-06-30 11:10:25 -0300

    teste

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-// var jshint = require('gulp-jshint');
+var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var convertEncoding = require('gulp-convert-encoding');
 
@@ -7,25 +7,25 @@ var convertEncoding = require('gulp-convert-encoding');
 
 
 // //jshint
-// gulp.task('jshint', function() {
-//     return gulp.src('*.js')
-//     		.pipe(jshint())
-//     		.pipe(jshint.reporter('default', {verbose: true}))
-//     		.pipe(jshint('.jshintrc'));
-// });
-
-
-//compressor
-gulp.task('uglify', function() {
-    gulp.src('*.js')
-    .pipe(convertEncoding({from: 'ISO-8859-1', to: 'UTF-8'}))
-    .pipe(uglify())
-    .pipe(convertEncoding({from: 'UTF-8', to: 'ISO-8859-1'}))
-    .pipe(gulp.dest('dist'));
+gulp.task('jshint', function() {
+    return gulp.src('*.js')
+    		.pipe(jshint())
+    		.pipe(jshint.reporter('default', {verbose: true}))
+    		.pipe(jshint('.jshintrc'));
 });
 
 
-gulp.task('default',['uglify'], function() {
+//compressor
+// gulp.task('uglify', function() {
+//     gulp.src('*.js')
+//     .pipe(convertEncoding({from: 'ISO-8859-1', to: 'UTF-8'}))
+//     .pipe(uglify())
+//     .pipe(convertEncoding({from: 'UTF-8', to: 'ISO-8859-1'}))
+//     .pipe(gulp.dest('dist'));
+// });
+
+
+gulp.task('default',['jshint'], function() {
     console.log('Deu Certo');
 });
 
